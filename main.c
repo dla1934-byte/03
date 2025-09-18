72d24b773e2981e121e121d70b9efe873168ccd6
dla1934-byte <dla1934@sookmyung.ac.kr>
2025-09-18 16:49:35 +0900

    03_4

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-    char c;
-    char next;
+    int ja, mo;
     
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("enter bunja :");
+    scanf("%d", &ja);
     
-    next = c + 1;
+    printf("enter bunmo :");
+    scanf("%d", &mo);
     
-    printf("The next character of %c (%i) is %c (%i).\n" , c, c, next, next);
-  
-  
+    printf("The result is %f\n", (float)ja/mo);
+    
+
   system("PAUSE");	
   return 0;
 }
